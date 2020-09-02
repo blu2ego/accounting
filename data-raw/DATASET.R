ba9b3ce77f743989bd1f0ea500f2886c0ba50370
blu2ego <blu2ego@gmail.com>
2020-09-03 03:37:49 +0900

    reconstruct source data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,5 +2,9 @@
 library(jsonlite)
 sample_accounting <- jsonlite::fromJSON("data-raw/sample-accounting.json")
 usethis::use_data(sample_accounting, overwrite = TRUE)
-audit <- load("data-raw/audit.RData")
-usethis::use_data(audit, overwrite = TRUE)
+
+external <- readRDS("data-raw/external_audit_from_biz.rds")
+usethis::use_data(external, overwrite = TRUE)
+
+internal <- readRDS("data-raw/internal_audit_from_biz.rds")
+usethis::use_data(internal, overwrite = TRUE)
